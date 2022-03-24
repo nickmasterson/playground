7ca877ea520429bd0353d69d6172364d2e4e56b5
nickmasterson <nickmasterson2018@gmail.com>
2022-03-23 21:18:24 -0500

    orbital_vector functions

---
+++ MAE568/orbital_vectors1.m
@@ -0,0 +1,11 @@
+function [r, v] = orbital_vectors1(p, e, theta)
+% this function will return the position and velocity vectors of an orbit
+% given the orbital elements
+% Perifocal frame only
+earth_radius = 6371000; % meters
+mu_earth = 3.986004418e14; 
+p = p*earth_radius; % ensure the input periapsis(p) had Earth-normalised units, otherwise this step is unecessary.
+r_scalar = p / (1+e*cosd(theta));
+r = [r_scalar*cosd(theta) r_scalar*sind(theta) 0];
+v = sqrt(mu_earth/p).*[-sind(theta) (e+cosd(theta)) 0];
+end
---
+++ MAE568/orbital_vectors2.m
@@ -0,0 +1,6 @@
+function [r, v] = orbital_vectors2(R_sez, lat, lon)
+% this function will return the position and velocity vectors of an orbit
+% given the orbital elements
+% topocentric frame only
+
+end
