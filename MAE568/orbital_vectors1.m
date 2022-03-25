3ea2d07469ef4bad8080c9e148de18e0cd507fef
nickmasterson <nickmasterson2018@gmail.com>
2022-03-24 21:26:27 -0500

    orbital vector function updates. working now

--- MAE568/orbital_vectors1.m
+++ MAE568/orbital_vectors1.m
@@ -1,4 +1,4 @@
-function [r, v] = orbital_vectors1(p, e, theta)
+function [r, v] = orbital_vectors1(p, e, theta, OMEGA, omega, i)
 % this function will return the position and velocity vectors of an orbit
 % given the orbital elements
 % Perifocal frame only
@@ -6,6 +6,9 @@ earth_radius = 6371000; % meters
 mu_earth = 3.986004418e14; 
 p = p*earth_radius; % ensure the input periapsis(p) had Earth-normalised units, otherwise this step is unecessary.
 r_scalar = p / (1+e*cosd(theta));
-r = [r_scalar*cosd(theta) r_scalar*sind(theta) 0];
-v = sqrt(mu_earth/p).*[-sind(theta) (e+cosd(theta)) 0];
+r_pqw = [r_scalar*cosd(theta); r_scalar*sind(theta); 0];
+v_pqw = sqrt(mu_earth/p).*[-sind(theta); (e+cosd(theta)); 0];
+R = peri2geo(OMEGA, omega, i);
+r = R*r_pqw;
+v = R*v_pqw;
 end
--- MAE568/orbital_vectors2.m
+++ MAE568/orbital_vectors2.m
@@ -1,6 +1,10 @@
-function [r, v] = orbital_vectors2(R_sez, lat, lon)
+function [r, v] = orbital_vectors2(R_sez, radar_lat, radar_lon, thetag)
 % this function will return the position and velocity vectors of an orbit
 % given the orbital elements
 % topocentric frame only
-
+theta = radar_lon + thetag;
+D = topo2geo(radar_lat, theta);
+D = D.';
+r = D * R_sez;
+% no formula for 'v' yet 
 end
